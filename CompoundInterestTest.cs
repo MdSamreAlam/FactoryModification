3f2cfc5db6e786259d004754648d25c1b8c28d38
MdSamreAlam <mohammedsamre.alam@augmentolabs.com>
2020-12-11 18:59:20 +0530

    UnitTestForSimpleInterestandCompoundInterest
    Created two seperate project for unit test. one name as SimpleInterestUnitTest ans Other CompoundInterestUnit test. Checked both condition like when test Fail Given message Failed and If pass send message test passed.

---
+++ CompoundInterestTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using CalculateInterestDemo;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
+
+namespace CompoundInterestTest
+{
+    /* 
+COPY RIGHT @AUGMENTO LABS 2020
+Created By Alam
+*/
+    [TestClass]
+    public class CompoundInterestTest
+    {
+        [Fact]
+        public void CompoundInterestUnit_Test()
+        {
+            double amount=1000;
+            int years = 5; ;
+            float interest=0.12f;
+            int annualfrequency=1;
+            Dictionary<string, double> Valuespair = new Dictionary<string, double>();
+            Valuespair.Add("amount", amount);
+            Valuespair.Add("years", years);
+            Valuespair.Add("rateOfIntrest", interest);
+            Valuespair.Add("annualfrequency", annualfrequency);
+            double expected = 1500;
+
+            //act
+
+            IInterestCalculator CompoundInterest = FactoryclassforInterest.GetInetrestCalculation(Valuespair, InterestCalculation.CompoundInteresCalculation);
+            var actual = CompoundInterest.Interest();
+            //Assert
+            if (actual == expected)
+            {
+                Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(actual, expected);
+            }
+            else
+            {
+                Microsoft.VisualStudio.TestTools.UnitTesting.Assert.Fail(" No matching value between actual and expected");
+            }
+
+        }
+    }
+}
---
+++ UnitTestForSimpleInterest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
+using CalculateInterestDemo;
+using System.Collections.Generic;
+
+namespace CalculateInterestUnitTest
+{
+    /* 
+COPY RIGHT @AUGMENTO LABS 2020
+Created By Alam
+*/
+    [TestClass]
+    public class TestInterestcalculation
+    {
+        [Fact]
+        public void Interest_Calculation_Test()
+        {
+
+            //arrange
+            double amount=1000;            
+            float rateOfInterest=0.05f;           
+            int years=3;          
+            Dictionary<string, double> Valuespair = new Dictionary<string, double>();
+            Valuespair.Add("amount", amount);
+            Valuespair.Add("years", years);
+            Valuespair.Add("rateOfInterest", rateOfInterest);
+            
+          double expected =  150.00000223517418;
+            
+            //act
+
+            IInterestCalculator SimpleInterest =  FactoryclassforInterest.GetInetrestCalculation(Valuespair, InterestCalculation.SimpleInterestCalculation);
+            var actual = SimpleInterest.Interest();
+            //Assert
+            if (actual == expected)
+            {
+                Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(actual, expected);
+            }
+            else
+            {
+                Microsoft.VisualStudio.TestTools.UnitTesting.Assert.Fail(" No matching value between actual and expected");
+            }
+          
+
+
+        }
+    }
+}
